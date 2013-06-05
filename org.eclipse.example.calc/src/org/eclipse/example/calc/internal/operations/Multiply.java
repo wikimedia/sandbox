0e85f3ec4d30a08153ee86eb05bf0654f42c707e
Alvaro Lopez Tijeras <alvaro.lopez.tijeras@ericsson.com>
2013-06-05 18:22:49 +0200

    Multiple for CodeReview
    Change-Id: I59dfd699cd85a307b20654ae99a22d4af8266ccf

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Multiply.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Multiply.java
@@ -18,7 +18,7 @@ public class Multiply extends AbstractOperation implements BinaryOperation {
 
 	@Override
 	public float perform(float arg1, float arg2) {
-		return arg1 / arg2;
+		return arg1 * arg2;
 	}
 
 	@Override
