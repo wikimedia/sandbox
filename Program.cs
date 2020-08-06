972cbaf68ddb26200b7e63df180f07ee9e764549
Ramil Yusupov <ramilyusupov228@gmail.com>
2020-08-06 09:16:47 +0500

    changes упущенная точка с запятой ! Change-Id: I26e49f6ad39bfc93a538219b1fcf39c14abff74b
    Change-Id: I332d93db22f0244467a782c3e7913c412b739d43

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
