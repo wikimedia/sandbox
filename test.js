bbf40dc35bf42dddf0522616d45273c934e92d5e
DMburugu <dmburugu@wikimedia.org>
2023-01-16 16:41:37 +0300

    - Fix loose string and logout separate value - Log out the value
    Change-Id: Iaf1ec9c41d7a3232adb7bd01680017521fb8e1f1

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
-this is a test
+let hello="this is a test"
 
 
 let message = "Gerrit test!"
 let tutorial = "new Message"
 
 message = "Testing testing testing"
+
+console.log(message)
+console.log(tutorial)
