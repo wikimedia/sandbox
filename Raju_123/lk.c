a63a4571bb5fe55ee5fc7dd8598ce2adaf783ab5
Mallikharjun <Mallikharjun.palasa@gytechs.com>
2023-02-08 14:47:01 +0530

    Raju
    Change-Id: I34ea8ca05280550b98dcad240b5ef48dc2ea3bee

---
+++ Raju_123/lk.c
@@ -0,0 +1,10 @@
+// subraction of two numbers
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int b = 30;
+
+    printf("the subtraction of two values is:%d",b-a);
+}
---
+++ Raju_123/ll.c
@@ -0,0 +1,13 @@
+// this is addition project
+#include <stdio.h>
+
+
+int main(){
+
+   int a  = 10;
+   int b = 20;
+
+   printf("the sum of two values is: %d ",a+b);
+
+}
+
